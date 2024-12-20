e385bce5b2253e2b6f3898bbeee1e4dc7f64cb99
Song-Gi-Sung <wlswlek123@naver.com>
2024-12-20 17:28:27 +0900

    doing add ci/cd railway

--- src/main/java/com/taskmanager/myapp/config/cors/CorsConfig.java
+++ src/main/java/com/taskmanager/myapp/config/cors/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.taskmanager.myapp.config.cors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,12 +12,15 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${allowed.origin}")
+    private String allowedOrigin;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", allowedOrigin));
         config.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
